79f2a396063ee0714553c6df05cbcfc2b2c56528
Luke Coffey <luke.m.coffey@gmail.com>
2022-04-13 11:59:28 +1000

    02 - A Fractal Tree - kinda get it but still so newb

--- src/index.js
+++ src/index.js
@@ -3,30 +3,84 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Contact } from './Contact.js'
-import { billionaires } from './billionaires.js'
+import { FractalHelpers } from './FractalHelpers.js'
 
-/*** put your `ContactList` component here ***/
-function ContactList(props) {
-  return (
-    <div className='ContactList'>
-      <h2 className='ContactList-title'>Contacts</h2>
-      {props.children}
-    </div>
-  )
+//Version 1 - their solution
+function FractalBox(props) {
+  if (props.depth > props.maxDepth) {
+    return null
+  }
+  else {
+    return (
+    <div>
+        <div style={FractalHelpers({
+          depth: props.depth,
+          heightFactor: 0.4,
+          lean: props.lean,
+          maxDepth: props.maxDepth,
+          side: props.side,
+        })}>
+          <FractalBox
+            depth = {props.depth + 1}
+            lean = {props.lean}
+            side = 'left'
+            sprout = {props.sprout}
+            size = {props.size}
+          />
+          <FractalBox
+            depth = {props.depth + 1}
+            lean = {props.lean}
+            side = 'right'
+            sprout = {props.sprout}
+            size = {props.size}
+          />
+      </div>
+      </div>
+    )
+  }
 }
 
-let contactElements = billionaires.map(billionaire =>
-  // The ... adds all properties from `billionaire`
-  // to the <Contact> element's props.
-  <Contact {...billionaire} key={billionaire.email} />
-)
+//Version 2 - my solution
+function FractalBox2(props) {
+  if (props.depth > props.maxDepth) {
+    return null
+  }
+  else {
+    return (
+    <div>
+        <div style={FractalHelpers({
+          depth: props.depth,
+          heightFactor: 0.4,
+          lean: 0,
+          maxDepth: props.maxDepth,
+          side: 'left',
+        })}>
+         <FractalBox depth={props.depth + 1} />
+        </div>
+        <div style={FractalHelpers({
+          depth: props.depth,
+          heightFactor: 0.4,
+          lean: 0,
+          maxDepth: props.maxDepth,
+          side: 'right',
+        })}>
+          <FractalBox depth={props.depth + 1} />
+        </div>
+      </div>
+    )
+  }
+}
 
+FractalBox.defaultProps = {
+  depth: 1,
+  maxDepth: 8,
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ContactList>{contactElements}</ContactList>
-  </React.StrictMode>,
+  <FractalBox
+    // You probably don't want to change this. See below for why.
+    maxDepth={8}
+  />,
   document.getElementById('root')
 );
 

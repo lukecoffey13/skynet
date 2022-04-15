33ac996983e8e8cf0cbc98fa5c3bef5246697733
Luke Coffey <luke.m.coffey@gmail.com>
2022-04-15 11:29:38 +1000

    04 - Sway With Mouse

--- src/index.js
+++ src/index.js
@@ -3,27 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { FractalHelpers } from './FractalHelpers.js'
-import { FractalTree } from './FractalTree'
+import FractalTreeBranch from './FractalTreeBranch'
 
-function getKeystroke(event) {
-  let inputValue = event.target.value
-  console.log(inputValue)
-  renderApp()
+// The height and width of the entire window
+const { innerHeight, innerWidth } = window
+
+// Store the current mouse position
+let mousePosition = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
 }
 
-function renderApp() {
+function renderFrame(event) {
+  // Compute "lean" and "sprout" based on time and current mouse position
+  let time = Date.now()
+  let fromHorizontalCenter = (innerWidth / 2 - mousePosition.x) / innerWidth
+  let fromVerticalCenter = (innerHeight / 2 - mousePosition.y) / innerHeight
+  let lean = 0.03 * Math.sin(time / 2000) + fromHorizontalCenter / 4
+  let sprout =
+    0.3 +
+    0.05 * Math.sin(time / 1300) +
+    fromVerticalCenter / 5 -
+    0.2 * Math.abs(0.5 - fromHorizontalCenter / 2)
+
   ReactDOM.render(
-    <input
-      onChange={getKeystroke}
-      placeholder='Type something...'
-      value=''
-    />,
-    document.getElementById('root')
+    <div
+      onMouseMove={(event) => {
+        mousePosition.x = event.clientX
+        mousePosition.y = event.clientY
+      }}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        overflow: 'hidden',
+      }}>
+      <FractalTreeBranch lean={lean} size={150} sprout={sprout} />
+    </div>,
+    document.getElementById('root'),
   )
+
+  // Schedule another frame
+  window.requestAnimationFrame(renderFrame)
 }
 
-renderApp()
+// Render the first frame
+renderFrame()
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
